af6285e4bee037faaad8e3cc49871ead92bf7f1b
FarLine99 <farline99@proton.me>
2025-10-27 11:42:23 +0300

    feat: инициализация архитектуры и базовых модулей проекта

---
+++ js/config/constants.js
@@ -0,0 +1,13 @@
+const CONSTANTS = {
+    RESUMES: [
+        {
+            id: "person-1",
+            name: "FarLine99",
+            x: 50,
+            y: 30,
+            path: "resumes/person-1/index.html",
+            avatar: "resumes/person-1/avatar.png",
+            icon: "resumes/person-1/icon.png",
+        },
+    ],
+};
---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    PointsModule.init();
+    ModalModule.init();
+});
